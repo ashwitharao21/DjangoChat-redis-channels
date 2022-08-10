5d3a6a0864f00717bc36c36fde8db02dca736c0f
Ashwitha A Rao <55918731+ashwitharao21@users.noreply.github.com>
2022-08-10 15:18:29 +0530

    Add files via upload

---
+++ restft/asgi.py
@@ -0,0 +1,36 @@
+"""
+ASGI config for restft project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+from django import urls
+
+from django.core.asgi import get_asgi_application
+from django.urls import re_path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from chat import consumers
+import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restft.settings')
+
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http':django_asgi_app,
+    'websocket': AllowedHostsOriginValidator( AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+            # [
+            #     # url(r'^ws/test-view', consumers.TestConsumer),
+            #     urls(r'ws/socket/',consumers.ChatConsumer),
+            # ]
+        )  
+    )),
+
+})
